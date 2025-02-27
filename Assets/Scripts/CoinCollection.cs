23d4f932711f45cd2d0242e86ad699c08f06e2ae
Chase <chasew13579@gmail.com>
2025-02-27 14:08:45 -0800

    Coin count script and text added

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 public class CoinCollection : MonoBehaviour
 {
     private int Coin = 0;
 
+    public TextMeshProUGUI coinCount;
 
     private void OnTriggerEnter(Collider other){
         if (other.transform.tag == "Coin"){
             Coin++;
+            coinCount.text = "Coin: "+ Coin.ToString();
             Debug.Log(Coin);
             Destroy(other.gameObject);
         }
